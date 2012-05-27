f4a7e3cdb7994f4525ea3036f07fef8d13da7534
palewire <ben.welsh@gmail.com>
2012-05-27 15:54:53 -0700

    Improved munin recipe slightly

--- chef/cookbooks/palewire/recipes/munin.rb
+++ chef/cookbooks/palewire/recipes/munin.rb
@@ -17,12 +17,29 @@ cookbook_file "/etc/munin/munin.conf" do
   group "root"
 end
 
-script "Restart Munin" do
+script "Zero out munin apache.conf" do
   interpreter "bash"
   user "root"
   group "root"
   code <<-EOH
     echo "#nothing to see here" > /etc/munin/apache.conf
+  EOH
+end
+
+script "Install python-munin" do
+  interpreter "bash"
+  user "root"
+  group "root"
+  code <<-EOH
+    pip install git+https://github.com/samuel/python-munin.git
+  EOH
+end
+
+script "Restart Munin" do
+  interpreter "bash"
+  user "root"
+  group "root"
+  code <<-EOH
     service munin-node restart
     service apache2 restart
   EOH
