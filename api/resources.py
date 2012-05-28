fe777d50ab9e4b9e5367e2ff01e7a9d71148874c
palewire <ben.welsh@gmail.com>
2012-05-28 07:02:50 -0700

    Added screenshot model to the API. More for ticket #6

--- api/resources.py
+++ api/resources.py
@@ -1,15 +1,33 @@
 from tastypie import fields
-from archive.models import Site, Update
 from django.conf import settings
 from tastypie.serializers import Serializer
 from django.core.urlresolvers import reverse
 from tastypie.resources import ModelResource
 from django.conf.urls.defaults import url
+from archive.models import Site, Update, Screenshot
 # Diff throttle depending on env
 if settings.PRODUCTION:
     from tastypie.throttle import CacheThrottle as Throttle
 else:
     from tastypie.throttle import BaseThrottle as Throttle
+
+
+class ScreenshotResource(ModelResource):
+    update = fields.ToOneField('api.resources.UpdateResource', 'update')
+    site = fields.ToOneField('api.resources.SiteResource', 'site')
+    
+    class Meta:
+        resource_name = 'screenshots'
+        queryset = Screenshot.objects.filter(site__status='active').select_related("update")
+        excludes = ['has_html', 'html_archived', 'html_raw']
+        allowed_methods = ['get',]
+        throttle = Throttle(throttle_at=50)
+        serializer = Serializer(formats=['json', 'jsonp'],
+            content_types = {
+                'json': 'text/javascript',
+                'jsonp': 'text/javascript'
+        })
+        include_absolute_url = True
 
 
 class SiteResource(ModelResource):
@@ -31,6 +49,7 @@ class SiteResource(ModelResource):
 
 
 class UpdateResource(ModelResource):
+    screenshots = fields.ToManyField('api.resources.ScreenshotResource', 'screenshot_set')
     
     class Meta:
         resource_name = 'updates'
--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from tastypie.api import Api
 from django.conf.urls.defaults import *
 
 api = Api(api_name='beta')
+api.register(resources.ScreenshotResource(), canonical=True)
 api.register(resources.SiteResource(), canonical=True)
 api.register(resources.UpdateResource(), canonical=True)
 
