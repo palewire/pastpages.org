b3a2a87e35a91903d6449797e7d9806d75f2a423
palewire <ben.welsh@gmail.com>
2012-08-18 23:31:42 -0700

    Drafted a pymunin plugin for rackspace cloud files. Fixes #83

--- chef/cookbooks/palewire/recipes/munin.rb
+++ chef/cookbooks/palewire/recipes/munin.rb
@@ -58,6 +58,34 @@ script "Install memcachedstats for PyMunin" do
   EOH
 end
 
+cookbook_file "/usr/share/munin/plugins/rackspacestats.py" do
+  source "rackspacestats.py"
+  mode "777"
+  owner "root"
+  group "root"
+end
+
+cookbook_file "/usr/share/munin/plugins/rackspaceinfo.py" do
+  source "rackspaceinfo.py"
+  mode "777"
+  owner "root"
+  group "root"
+end
+
+script "Install rackspacestats for PyMunin" do
+  interpreter "bash"
+  user "root"
+  group "root"
+  code <<-EOH
+    echo "[rackspacestats]
+    env.username #{node[:rackspace_user]}
+    env.api_key #{node[:rackspace_apikey]}
+    env.container #{node[:rackspace_container]}" > /etc/munin/plugin-conf.d/rackspacestats
+    ln -s /usr/share/munin/plugins/rackspacestats.py /etc/munin/plugins/rackspacestats
+    chmod 0755 /etc/munin/plugins/rackspacestats
+  EOH
+end
+
 script "Restart Munin" do
   interpreter "bash"
   user "root"
