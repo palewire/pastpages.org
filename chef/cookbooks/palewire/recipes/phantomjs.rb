4b121eea716987a978201c5a4d789cb089ef001b
palewire <ben.welsh@gmail.com>
2013-01-19 18:15:21 -0800

    Reworked the postgresql recipe and removed some we wont want for this new deploy

--- chef/cookbooks/palewire/recipes/phantomjs.rb
+++
@@ -1,1 +0,0 @@
-ln -s /home/ben/Code/palewire/pastpages.org/repo/phantomjs/bin/phantomjs /home/ben/Code/palewire/pastpages.org/bin/phantomjs
--- chef/cookbooks/palewire/recipes/postgresql.rb
+++ chef/cookbooks/palewire/recipes/postgresql.rb
@@ -1,15 +1,41 @@
-include_recipe "postgresql::postgis"
+# Intended to work on Ubuntu 12.04
 
-postgresql_user node[:db_user] do
-  password node[:db_password]
-  privileges :superuser => true, :createdb => true, :inherit => true, :login => true
+package "binutils" do
+    :upgrade
 end
 
-postgresql_database node[:db_name] do
-  user node[:db_user]
+package "gdal-bin" do
+    :upgrade
+end
+
+package "libproj-dev" do
+    :upgrade
+end
+
+package "postgresql-9.1-postgis" do
+    :upgrade
+end
+
+package "postgresql-server-dev-9.1" do
+    :upgrade
+end
+
+package "python-psycopg2" do
+    :upgrade
+end
+
+cookbook_file "/tmp/create_postgis_template.sh" do
+  source "postgresql/create_postgis_template.sh"
+  mode 0640
   owner "postgres"
-  languages [ "plpgsql" ]
-  action :create
+  group "postgres"
 end
 
-
+script "create-postgis-template" do
+  interpreter "bash"
+  user "postgres"
+  cwd "/tmp"
+  code <<-EOH
+  source ./create_postgis_template.sh
+  EOH
+end
