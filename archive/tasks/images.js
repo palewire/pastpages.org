27f68bb19432b76dfcce0fbbac2e3717761ee02c
palewire <ben.welsh@gmail.com>
2013-02-05 07:35:34 -0800

    Upped page wait to 5 seconds

--- archive/tasks/images.js
+++ archive/tasks/images.js
@@ -1,7 +1,7 @@
 var page = require('webpage').create();
 var address = phantom.args[0];
 var outfile = phantom.args[1];
-var secondsToWait = 3;
+var secondsToWait = 5;
 page.viewportSize = { width: 1024, height: 768 };
 page.open(address, function (status) {
     if (status !== 'success') {
