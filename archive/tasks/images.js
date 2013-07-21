b914b961d2984a77afe7f1d843191c79252a2ec3
palewire <ben.welsh@gmail.com>
2013-07-21 13:25:18 -0700

    Presetting a white background on all pages to prevent transparent bgcolor pages from coming out black in JPG screenshots. For #142.

--- archive/tasks/images.js
+++ archive/tasks/images.js
@@ -3,11 +3,21 @@ var address = phantom.args[0];
 var outfile = phantom.args[1];
 var secondsToWait = 5;
 page.viewportSize = { width: 1024, height: 768 };
+page.evaluate(function() {
+    document.body.bgColor = 'white';
+});
 page.open(address, function (status) {
     if (status !== 'success') {
         console.log('Unable to load the address!');
         phantom.exit();
     } else {
+        page.evaluate(function() {
+          var style = document.createElement('style'),
+              text = document.createTextNode('body { background: #fff }');
+          style.setAttribute('type', 'text/css');
+          style.appendChild(text);
+          document.head.insertBefore(style, document.head.firstChild);
+        });
         window.setTimeout(function () {
             page.render(outfile);
             phantom.exit();
