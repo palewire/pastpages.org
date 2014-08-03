6d668b0458bababe90808508c444ed1a21b14ccd
palewire <ben.welsh@gmail.com>
2014-08-02 21:38:28 -0700

    Added new relic application integration. Fixes #155.

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,9 +1,16 @@
 import os, sys
+import newrelic.agent
+
 sys.path.append('/apps/pastpages.org/')
 sys.path.append('/apps/pastpages.org/repo/')
 sys.path.append('/apps/pastpages.org/lib/python2.7/site-packages/')
 sys.path.append('/apps/pastpages.org/bin/')
+
+newrelic.agent.initialize('/apps/pastpages.org/repo/project/newrelic.ini')
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 os.environ["CELERY_LOADER"] = "django"
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
