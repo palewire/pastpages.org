dd28610c55a9e0748148283e5d7d415ad0f34f81
palewire <ben.welsh@gmail.com>
2014-11-29 20:10:56 -0800

    Added new style logging to the archive task. Fixes #143.

--- archive/admin.py
+++ archive/admin.py
@@ -1,31 +1,53 @@
 from django.contrib import admin
-from archive.models import Site, Update, Screenshot, Champion
+from archive.models import Site, Update, Screenshot, Champion, ScreenshotLog
 
 
 class SiteAdmin(admin.ModelAdmin):
-    list_display = ("name", "status", "has_html_screenshots", "on_the_homepage")
+    list_display = (
+        "name",
+        "status",
+        "has_html_screenshots",
+        "on_the_homepage"
+    )
     list_filter = ("status",)
     prepopulated_fields = {"slug": ("name",)}
     list_editable = ('status', 'has_html_screenshots', 'on_the_homepage')
-
-admin.site.register(Site, SiteAdmin)
 
 
 class UpdateAdmin(admin.ModelAdmin):
     list_display = ('start',)
     date_hierarchy = 'start'
 
-admin.site.register(Update, UpdateAdmin)
-
 
 class ScreenshotAdmin(admin.ModelAdmin):
     list_display = ("site", "update",)
     readonly_fields = ('site', 'update', 'timestamp')
 
-admin.site.register(Screenshot, ScreenshotAdmin)
-
 
 class ChampionAdmin(admin.ModelAdmin):
     pass
 
+
+class ScreenshotLogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "update",
+        "site",
+        "message_type",
+        "message"
+    )
+    list_filter = ("message_type", "site")
+    readonly_fields = (
+        "update",
+        "site",
+        "screenshot",
+        "message_type",
+        "message"
+    )
+
+
+admin.site.register(Site, SiteAdmin)
+admin.site.register(Update, UpdateAdmin)
+admin.site.register(Screenshot, ScreenshotAdmin)
 admin.site.register(Champion, ChampionAdmin)
+admin.site.register(ScreenshotLog, ScreenshotLogAdmin)
