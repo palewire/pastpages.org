bcec375a83060e90d6d60ee8ff1a19a338bb9d0a
Ben Welsh <ben.welsh@gmail.com>
2016-02-08 13:52:49 -0800

    etwas immer

--- archive/managers.py
+++ archive/managers.py
@@ -58,7 +58,7 @@ class UpdateManager(models.Manager):
              SELECT id, start
              FROM archive_update
              ORDER BY start DESC
-             LIMIT 25
+             LIMIT 100
             ) as u
             INNER JOIN archive_screenshot as s
             ON u.id = s.update_id
